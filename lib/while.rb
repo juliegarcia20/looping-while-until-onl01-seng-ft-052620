2f6c9e8c3ebd7f55fc3e3397999891ac53547f0c
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-01 21:09:44 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,19 @@
 def using_while
-  levitation_force = 6
-	
+levitation_force = 6
+while levitation_force < 10 
+puts "Wingardium Leviosa"
+levitation_force += 1
   #your code here
-end
+end 
+end 
 
 
+
+
+#while num_of_hotdogs_eaten < 7
+ # num_of_hotdogs_eaten += 1
+ # puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+#end
+# => nil (return value)
+ 
+#puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
