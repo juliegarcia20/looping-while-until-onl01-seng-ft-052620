454ae45ddcdefa63ab8a1f1e8a7328ca4e98a3dc
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-01 20:54:59 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,18 @@
 def using_while
-  levitation_force = 6
-	
+while levitation_force < 10 
+puts "Wingardium Leviosa"
+
   #your code here
 end
+end 
 
 
+
+
+#while num_of_hotdogs_eaten < 7
+ # num_of_hotdogs_eaten += 1
+ # puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+#end
+# => nil (return value)
+ 
+#puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
